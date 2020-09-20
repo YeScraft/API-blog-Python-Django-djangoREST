219a0e1a0a08314ba83bcef0894293b22381d9a7
YeScraft <6007833@gmail.com>
2020-09-20 22:39:24 +0300

    deploy4

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'ieztj=$r7e=_#7%4ldq)=f_)^k11%(s@i7-mve5+c=kkm@2w3p'
-SECRET_KEY = os.environ.get('SECRET_KEY', 'please-set-secret-key-through-env')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
